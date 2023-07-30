b622b4adcb4fbf277ae85ca1f14122b4a5bd5114
SonalDiksura <140968617+SonalDiksura@users.noreply.github.com>
2023-07-30 23:44:07 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function validateForm(){
+    let name = document.getElementById('username').value;
+    let pass = document.getElementById('pass').checked;
+
+
+    if(name == "" && pass == "" && pass.length < 8){
+        window.alert("Plaese check atleast one value");
+    }
+    else{
+        window.alert("Form submitted successfully")
+    }
+}
+
+function resetForm()
+ {
+    document.getElementById('form2').reset();
+  }
